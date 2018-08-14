d12af042ada8f3e0f86b1f8af2b454b5e1e06591
barley814 <yuan940814>
2018-08-14 15:26:59 +0800

    resnet

---
+++ resnet/resnet.py
@@ -0,0 +1,122 @@
+import tensorflow as tf
+
+
+class Resnet(object):
+    def __init__(self, depth, num_classes):
+        self.depth = depth
+        self.num_classes = num_classes
+
+    def model(self, x):
+        with tf.variable_scope('ResNet'):
+            net = tf.layers.conv2d(inputs=x,
+                                   filters=64,
+                                   kernel_size=7,
+                                   strides=2,
+                                   padding='same',
+                                   activation=tf.nn.relu,
+                                   name='conv1',
+                                   )
+            net = tf.layers.max_pooling2d(inputs=net,
+                                          pool_size=3,
+                                          strides=2,
+                                          padding='same',
+                                          name='maxpool1')
+            if self.depth == 18:
+                net = self.block(net, 64, 2, 'block2', 'shallow', strides=1)
+                net = self.block(net, 128, 2, 'block3', 'shallow')
+                net = self.block(net, 256, 2, 'block4', 'shallow')
+                net = self.block(net, 512, 2, 'block5', 'shallow')
+            elif self.depth == 34:
+                net = self.block(net, 64, 3, 'block2', 'shallow', strides=1)
+                net = self.block(net, 128, 4, 'block3', 'shallow')
+                net = self.block(net, 256, 6, 'block4', 'shallow')
+                net = self.block(net, 512, 3, 'block5', 'shallow')
+            elif self.depth == 50:
+                net = self.block(net, 256, 3, 'block2', 'deep', strides=1)
+                net = self.block(net, 512, 4, 'block3', 'deep')
+                net = self.block(net, 1024, 6, 'block4', 'deep')
+                net = self.block(net, 2048, 3, 'block5', 'deep')
+            elif self.depth == 101:
+                net = self.block(net, 256, 3, 'block2', 'deep', strides=1)
+                net = self.block(net, 512, 4, 'block3', 'deep')
+                net = self.block(net, 1024, 23, 'block4', 'deep')
+                net = self.block(net, 2048, 3, 'block5', 'deep')
+            elif self.depth == 152:
+                net = self.block(net, 256, 3, 'block2', 'deep', strides=1)
+                net = self.block(net, 512, 8, 'block3', 'deep')
+                net = self.block(net, 1024, 36, 'block4', 'deep')
+                net = self.block(net, 2048, 3, 'block5', 'deep')
+            else:
+                raise ValueError("Expected argument 'deep' must be in [18, 34, 50, 101, 152].")
+
+            net = tf.layers.average_pooling2d(inputs=net,
+                                              pool_size=7,
+                                              strides=1,
+                                              name='avgpool1'
+                                              )
+            net = tf.layers.flatten(inputs=net)
+            self.logits = tf.layers.dense(inputs=net,
+                                     units=self.num_classes,
+                                     name='logits',
+                                     )
+            self.prob = tf.nn.softmax(self.logits, name='prob')
+
+    def block(self, x, n_out, n_bottleneck, scope, mode, strides=2):
+        with tf.variable_scope(scope):
+            out = self.bottleneck(x, n_out=n_out, scope="bottleneck1", mode=mode, strides=strides)
+            for i in range(1, n_bottleneck):
+                out = self.bottleneck(out, n_out, scope="bottleneck{0}".format(i + 1), mode=mode)
+        return out
+
+    def bottleneck(self, x, n_out, scope, mode, strides=1):
+        n_in = x.get_shape()[-1]
+        n_first = n_out / 4
+        with tf.variable_scope(scope):
+            if mode == 'shallow':
+                f = tf.layers.conv2d(inputs=x,
+                                     filters=n_out,
+                                     kernel_size=3,
+                                     strides=strides,
+                                     padding='same',
+                                     activation=tf.nn.relu,
+                                     name='conv1'
+                                     )
+                f = tf.layers.conv2d(inputs=f,
+                                     filters=n_out,
+                                     kernel_size=3,
+                                     strides=1,
+                                     padding='same',
+                                     activation=None,
+                                     name='conv2'
+                                     )
+            elif mode == 'deep':
+                f = tf.layers.conv2d(inputs=x,
+                                     filters=n_first,
+                                     kernel_size=1,
+                                     strides=strides,
+                                     name='conv1',
+                                     )
+                f = tf.layers.conv2d(inputs=f,
+                                     filters=n_first,
+                                     kernel_size=3,
+                                     strides=1,
+                                     name='conv2',
+                                     )
+                f = tf.layers.conv2d(inputs=f,
+                                     filters=n_out,
+                                     kernel_size=1,
+                                     strides=1,
+                                     name='conv3')
+            else:
+                raise ValueError("expected argument 'mode' must be between 'shallow' and 'deep'.")
+
+            if n_in != n_out:  # projection
+                shortcut = tf.layers.conv2d(inputs=x,
+                                            filters=n_out,
+                                            kernel_size=1,
+                                            strides=2,
+                                            name='projection')
+            else:
+                shortcut = x  # identical mapping
+            out = tf.nn.relu(shortcut + f)
+        return out
---
+++ resnet/train.py
@@ -0,0 +1,148 @@
+import tensorflow as tf
+import resnet
+import utils
+import os
+import math
+
+
+def main():
+    # Training and test data
+    data_path = '/path/to/your/data'
+    test_ratio = 0.1
+
+    # Hyper-parameters
+    # Net structure
+    depth = 18
+    num_classes = 2
+    # Training parameters
+    optimizer = 'sgd'        # A string from: 'agd', 'adam', 'momentum'
+    learning_rate = 1e-4
+    momentum = None          # Necessary if optimizer is 'momentum'
+    batch_size = 32
+    epochs = 500
+    epochs_every_test = 100
+    epochs_every_save = 100
+    method = 'restart'       # A string from: 'restart', 'restore'
+
+    #  Log file setting
+    trained_model_directory = 'directory/to/your/restored/model' # Necessary if method is "restore"
+    model_directory = 'path/to/save/model'
+    utils.create_directory(model_directory)
+    model_name = 'resnet%d_%s_%s.ckpt' % (depth, optimizer, utils.record_time())
+    model_path = os.path.join(model_directory, model_name)
+    log_directory = 'directory/to/save/log_files'
+    utils.create_directory(log_directory)
+    log_filename = 'resnet%d_%s_%s.log' % (depth, optimizer, utils.record_time())
+    summarize = True         # True if summarize in tensorboard
+
+    # Load data and preprocess it
+    train_list, test_list = utils.train_test_split(data_path, test_ratio)
+    num_train_sample = utils.cal_num(train_list)
+    num_test_sample = utils.cal_num(test_list)
+    train_filenames, train_labels = utils.get_filename_label(data_path, train_list)
+    test_filenames, test_labels = utils.get_filename_label(data_path, test_list)
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_filenames, train_labels)).map(
+        lambda x, y: utils._parse_function(x, y, num_classes)).shuffle(
+        buffer_size=1024, reshuffle_each_iteration=True).batch(batch_size)
+    test_dataset = tf.data.Dataset.from_tensor_slices((test_filenames, test_labels)).map(
+        lambda x, y: utils._parse_function(x, y, num_classes)).batch(batch_size).repeat(1)
+    iterator = tf.data.Iterator.from_structure(train_dataset.output_types, train_dataset.output_shapes)
+    example_batch, label_batch = iterator.get_next()
+    train_init_op = iterator.make_initializer(train_dataset)
+    test_init_op = iterator.make_initializer(test_dataset)
+
+    # Define input x and output y
+    x = tf.placeholder('float', shape=[None, 224, 224, 3], name='x')
+    y_ = tf.placeholder('float', shape=[None, num_classes], name='y')
+    resnet = resnet.Resnet(depth, num_classes)
+    resnet.model(x)
+    train_op, correct_count, loss, accuracy = utils.train(resnet.logits, resnet.prob, y_, optimizer, learning_rate, momentum)
+
+    # Summarize in tensorboard
+    if summarize:
+        tf.summary.scalar('loss', loss)
+        tf.summary.scalar('accuracy', accuracy)
+        summary_op = tf.summary.merge_all()
+
+    saver = tf.train.Saver()
+
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
+        # logs writer
+        if summarize:
+            writer = tf.summary.FileWriter(log_directory, sess.graph)
+        fo = open(os.path.join(log_directory, log_filename), 'w')
+
+        if method == 'restart':
+            init_op = tf.global_variables_initializer()
+            sess.run(init_op)
+        elif method == 'restore':
+            saver.restore(sess, tf.train.latest_checkpoint(trained_model_directory))
+            print("Model restored...", file=fo)
+        else:
+            raise ValueError("Invalid METHOD parameter, please input 'restart' or 'restore'.")
+
+        print('%d training images and %d test images' % (num_train_sample, num_test_sample), file=fo)
+        step = 0
+        m = math.ceil(num_train_sample / batch_size)
+        for i in range(epochs):
+            sess.run(train_init_op)
+            train_acc_ = 0.0
+            train_loss_ = 0.0
+            try:
+                while True:
+                    batch_X, batch_Y = sess.run([example_batch, label_batch])
+                    if summarize:
+                        _, acc_, loss_, summary= sess.run([train_op, accuracy, loss, summary_op],
+                                                             feed_dict={x: batch_X, y_: batch_Y})
+                        writer.add_summary(summary)
+                    else:
+                        _, acc_, loss_ = sess.run([train_op, accuracy, loss],
+                                                  feed_dict={x: batch_X, y_: batch_Y})
+                    train_acc_ += acc_
+                    train_loss_ += loss_
+                    step += 1
+            except tf.errors.OutOfRangeError:
+                train_accuracy = train_acc_ / m
+                train_loss = train_loss_ / m
+                print("%s: epoch %d, train_accuracy = %f, train_loss = %f."
+                              % (utils.print_time(), i, train_accuracy, train_loss), file=fo)
+            # test
+            if (i + 1) % epochs_every_test == 0:
+                sess.run(test_init_op)
+                total_correct_count = 0
+                test_loss_ = 0.0
+                try:
+                    while True:
+                        X_test, Y_test = sess.run([example_batch, label_batch])
+                        true_count, current_loss = sess.run([correct_count, loss], feed_dict={x: X_test, y_: Y_test})
+                        total_correct_count += true_count
+                        test_loss_ += current_loss
+                except tf.errors.OutOfRangeError:
+                    test_accuracy = total_correct_count / num_test_sample
+                    test_loss = test_loss_ / m
+                    print("%s: epoch %d, test_accuracy = %f, test_loss = %f" % (utils.print_time(), i, test_accuracy, test_loss), file=fo)
+            if (i + 1) % epochs_every_save == 0:
+                saver.save(sess, model_path, global_step=step)
+        print("Done training -- epoch limited reached")
+        # test
+        sess.run(test_init_op)
+        total_correct_count = 0
+        test_loss_ = 0.0
+        try:
+            while True:
+                X_test, Y_test = sess.run([example_batch, label_batch])
+                true_count, current_loss = sess.run([correct_count, loss], feed_dict={x: X_test, y_: Y_test})
+                total_correct_count += true_count
+                test_loss_ += current_loss
+        except tf.errors.OutOfRangeError:
+            print("Done test！", file=fo)
+            test_accuracy = total_correct_count / num_test_sample
+            test_loss = test_loss_ / m
+            print("Test_accuracy = %f, test_loss = %f" % (test_accuracy, test_loss), file=fo)
+
+        fo.close()
+        saver.save(sess, model_path, global_step=step)
+
+
+if __name__ == '__main__':
+    main()
